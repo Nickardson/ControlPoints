8ed89b5196a9655ca1f2ce7b538117251629e5c5
Nickardson <taylorgratzer@yahoo.com>
2013-04-11 11:57:42 -0400

    Moved commands to their own seperate class

--- CaptureTheFlag/src/tamerial/ctf/CaptureTheFlag.java
+++ CaptureTheFlag/src/tamerial/ctf/CaptureTheFlag.java
@@ -16,14 +16,19 @@ public class CaptureTheFlag extends JavaPlugin implements Listener {
 	public Game game;
 	double accumulatedTicks = 0;
 	public EventListener eventListener;
+	public CommandListener commandListener;
 	
 	@Override
     public void onEnable(){
 		this.saveDefaultConfig();
 		
 		game = new Game(new Teams(), new CapturePoints());
+		
 		eventListener = new EventListener();
 		eventListener.setGame(game);
+		
+		commandListener = new CommandListener();
+		commandListener.setGame(game);
 		
 		if (getConfig().getString("ctf.world") != null)
 			Game.world = getConfig().getString("ctf.world");
@@ -200,118 +205,6 @@ public class CaptureTheFlag extends JavaPlugin implements Listener {
     }
     
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-    	if (cmd.getName().equalsIgnoreCase("start")) {
-    		game.gameOverTicks = 29;
-    		game.forceStart = true;
-    		
-    		return true;
-    	}
-    	else if (cmd.getName().equalsIgnoreCase("pause")) {
-    		game.forcePause = !game.forcePause;
-    		sender.sendMessage(ChatColor.GREEN + "Game " + (game.forcePause ? "paused" : "unpaused") + "!");
-    		
-    		return true;
-    	}
-    	else if (cmd.getName().equalsIgnoreCase("progress")) {
-    		if (args.length >= 2) {
-    			try {
-	    			int capturePointId = Integer.parseInt(args[0]);
-	    			double progress = Double.parseDouble(args[1]);
-	    			game.getCapturePoints().get(capturePointId).setCaptureProgress(progress);
-	    			
-	    			return true;
-    			} 
-    			catch (Exception err) {
-    				sender.sendMessage(ChatColor.RED + "Failed to execute, is the flag ID out of bounds?" + ChatColor.RESET);
-    				return false;
-    			}
-    		}
-    	}
-    	else if (cmd.getName().equalsIgnoreCase("join")) {
-    		if (sender instanceof Player) {
-    			if (args.length >= 1) {
-    				if (args[0].trim().equalsIgnoreCase("blue")) {
-    					//if (game.teams.getPlayersOnTeam(-1).size() <= game.teams.getPlayersOnTeam(1).size()) {
-	    					game.teams.setTeam(game, sender.getName(), -1);
-	    					sender.sendMessage("You have joined the " + ChatColor.BLUE + "Blue" + ChatColor.RESET + " team!");
-	    					
-	    					return true;
-    					//}
-    					//else {
-    					//	sender.sendMessage(ChatColor.RED + "You cannot make the teams unbalanced by more than 1 player." + ChatColor.RESET);
-    					//}
-    				}
-    				else if (args[0].trim().equalsIgnoreCase("red")) {
-    					//if (game.teams.getPlayersOnTeam(1).size() <= game.teams.getPlayersOnTeam(-1).size()) {
-    	    				game.teams.setTeam(game, sender.getName(), 1);
-    	    				sender.sendMessage("You have joined the " + ChatColor.RED + "Red" + ChatColor.RESET + " team!");
-    	    				
-    	    				return true;
-	    				//}
-						//else {
-						//	sender.sendMessage(ChatColor.RED + "You cannot make the teams unbalanced by more than 1 player." + ChatColor.RESET);
-						//	return true;
-						//}
-    				}
-    				else {
-    					return false;
-    				}
-    			}
-    		}
-    	}
-    	else if (cmd.getName().equalsIgnoreCase("forcejoin")) {
-    		if (args.length >= 2) {
-	    		try {
-	    			game.teams.setTeam(game, args[0], args[1].trim().equalsIgnoreCase("blue") ? -1 : 1);
-	    			
-	    			return true;
-	    		}
-	    		catch (Exception err) {
-	    			sender.sendMessage(ChatColor.RED + "Forcejoin failed.  Malformed team number?" + ChatColor.RESET);
-	    		}
-    		}
-    		
-    		return false;
-    	}
-    	else if (cmd.getName().equalsIgnoreCase("class")) {
-    		if (sender instanceof Player) {
-    			if (args.length >= 1) {
-    				game.changeClass((Player)sender, args[0]);
-    			}
-    			else {
-    				String list = "";
-    				for (String outfit : game.outfits.keySet()) {
-    					list += outfit + ", ";
-    				}
-    				
-    				list = list.substring(0, list.length()-2);
-    				
-    				sender.sendMessage("Available classes: ");
-    				sender.sendMessage(list);
-    				
-    				return true;
-    			}
-    		}
-    	}
-    	else if (cmd.getName().equalsIgnoreCase("gametime")) {
-    		if (args.length >= 1) {
-    			try {
-    				int time = Integer.parseInt(args[0]);
-    				
-    				if (time < 0) {
-    					throw new NumberFormatException();
-    				}
-    				
-    				game.timeLeft = time;
-    				
-    				return true;
-    			}
-    			catch (NumberFormatException err) {
-    				sender.sendMessage(ChatColor.RED + "Could not set game time.  Make sure gametime is a positive integer.");
-    				return false;
-    			}
-    		}
-    	}
-    	return false; 
+    	return commandListener.handleCommand(sender, cmd, label, args);
     }
 }
